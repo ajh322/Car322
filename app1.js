0f215934674320af004192628c0692fef61f47ae
AJH322 <jihwan322@gmail.com>
2017-05-05 18:12:35 +0900

    moved

--- app1.js
+++ app1.js
@@ -4,7 +4,11 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var fs = require('fs');
-
+app.set('port', process.env.PORT || 8080);
+app.get('/', function (req, res) {
+    console.log("come");
+    res.end();
+});
 app.listen(app.get('port'), function () {
     console.log("going");
 })
